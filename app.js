ebda642c86efe97796776b2c100e349178744782
sriramsri292 <133184406+sriramsri292@users.noreply.github.com>
2023-10-28 18:40:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app_server=express();
 const cors = require('cors');
-app_server.use(cors({ origin: 'https://653cc7af7e10805ff1a988ef--magenta-griffin-93a869.netlify.app/' }));
+app_server.use(cors({ origin: '*' }));
 app_server .use(express.json());
 app_server.use("/users",  require("./controllers/users.controller"));
 app_server.use("/auth",require("./controllers/auth.controller"));
